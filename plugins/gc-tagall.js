1d53fd8e2ac12feb28070b332f33baf0a6d8f293
Don Michifuz <Arm0899@outlook.es>
2025-03-18 14:39:41 -0600

    V3

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,27 +1,28 @@
 
 
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-    if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn)
-    throw false
-    }
-    let pesan = args.join` `
-    let oi = `_*BOT By Arm*_
-    
-     ${pesan}`
-    let teks = `━━━━━━━━━━━━━━━━━━━━
-     ${oi}\n\n DON MICHIFUZ LES HABLA
-     ━━━━━━━━━━━━━━━━━━━━\n`
-    for (let mem of participants) {
-    teks += `🔥 @${mem.id.split('@')[0]}\n`}
-    teks += ` @ Don Michifuz Bot `
-    conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-    }
-    handler.help = ['tagall <mesaje>','invocar <mesaje>']
-    handler.tags = ['group']
-    handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
-    handler.admin = true
-    handler.group = true
-    export default handler
-  
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/es.json`))
+  const tradutor = _translate.plugins.gc_tagall
+
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `${global.ig} ${pesan}`;
+  let teks = `*${global.etiqueta}*\n${oi}\n\n*🌤️𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`;
+  for (const mem of participants) {
+    teks += `${global.emojietiqueta} @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `${global.packname}`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
