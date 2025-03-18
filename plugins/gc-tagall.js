88d7f3dbe446491e9c35ae14e3714fbc97dbdc8b
Don Michifuz <Arm0899@outlook.es>
2025-03-18 14:41:19 -0600

    V2

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,9 +1,7 @@
-
-
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
   const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/es.json`))
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.gc_tagall
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
@@ -12,12 +10,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `${global.ig} ${pesan}`;
-  let teks = `*${global.etiqueta}*\n${oi}\n\n*🌤️𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`;
+  const oi = `${tradutor.texto1[0]} ${pesan}`;
+  let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
   for (const mem of participants) {
-    teks += `${global.emojietiqueta} @${mem.id.split('@')[0]}\n`;
+    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `${global.packname}`;
+  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
