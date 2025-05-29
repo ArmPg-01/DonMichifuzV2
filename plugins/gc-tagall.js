6eb707c7605a860a047823c3f936db8d085dfb4a
Don Michifuz <Arm0899@outlook.es>
2025-05-29 17:48:28 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,32 +1,18 @@
-
-// Tagall Mejorado por willzek
-import fetch from 'node-fetch';
-import PhoneNumber from 'awesome-phonenumber';
-
-const handler = async (m, { participants, args }) => {
-  const pesan = args.join` `;
-  const oi = `*» INFO :* ${pesan}`;
-  let mensajes = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n`;
-
-  for (const mem of participants) {
-    let numero = PhoneNumber('+' + mem.id.replace('@s.whatsapp.net', '')).getNumber('international');
-    let api = `https://delirius-apiofc.vercel.app/tools/country?text=${numero}`;
-    let response = await fetch(api);
-    let json = await response.json();
-
-    let paisdata = json.result ? json.result.emoji : '🔥';
-    mensajes += `${paisdata} @${mem.id.split('@')[0]}\n`;
-  }
-
-    mensajes += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
-
-  conn.sendMessage(m.chat, { text: mensajes, mentions: participants.map((a) => a.id) });
-};
-
-handler.help = ['todos *<mensaje opcional>*'];
-handler.tags = ['grupo'];
-handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-
-export default handler;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+    if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn)
+    throw false
+    }
+    let pesan = args.join` `
+    let oi = `*MENSAJE:* ${pesan}`
+    let teks = `*DON MICHIFUZ LOS INVOCA*\n${oi}\n\n*MENCIONES*\n`
+    for (let mem of participants) {
+    teks += `👻 @${mem.id.split('@')[0]}\n`}
+    teks += `${wm}`
+    conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
+    }
+    handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+    handler.admin = true
+    handler.group = true
+    handler.botAdmin = true
+    export default handler
